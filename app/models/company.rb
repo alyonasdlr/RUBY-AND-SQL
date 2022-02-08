0274b8f79fb43c77da456b8655961ce98960b4fc
alyonasdlr <elena.avdeeva@kellogg.northwestern.edu>
2022-02-08 05:08:28 +0000

    finished

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,7 @@
 class Company < ApplicationRecord
+   # def contacts
+   #     Contact.where({company_id: id})
+   # end
+has_many :contacts
+
 end
--- labs/2-associations.rb
+++ labs/2-associations.rb
@@ -4,6 +4,21 @@
 # rails runner labs/2-associations.rb
 
 # 1. write code to display each contact (you can reuse the previous lab's code) and include the contact's company name, e.g.:
+apple = Company.where({name: "Apple"})[0]
+apple_id = apple.id
+new_contact = Contact.new
+new_contact.first_name = "Andy"
+new_contact.last_name = "Jassy"
+new_contact.email = "andy.jassy@amazon.com"
+new_contact.company_id = apple_id
+new_contact.save
+
+craig = Contact.new
+craig.first_name = "Craig"
+craig.last_name = "Federighi"
+craig.email = "craig@apple.com"
+craig.company_id = apple_id
+craig.save
 
 # ---------------------------------
 # Contacts: 4
@@ -14,6 +29,21 @@
 
 # 2. similar to above, but this time organized by company, write code to display each company (name) and its contacts, e.g.:
 
+
+#puts "Apple"
+#all_contacts = Contact.all
+#for contact in all_contacts
+#    puts "#{contact.first_name} #{contact.last_name} - #{contact.company.name}"
+#end  
+
+
+
+for company in Company.all
+    puts company.name
+    for contact in company.contacts
+        puts " #{contact.first_name} "
+    end
+end   
 # ---------------------------------
 # Apple Inc.
 # Tim Cook - tim@apple.com
